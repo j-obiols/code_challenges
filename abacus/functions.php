9ffab022e57d6909f9a18e03c374101329f62edb
Joana Obiols <joana.obiols@gmail.com>
2023-10-21 18:50:35 +0200

    Refactoring

--- abacus/functions.php
+++ abacus/functions.php
@@ -3,7 +3,7 @@
 
 function drawAbacus(): array {
         
-    $abacus = array();
+    $abacus = [];
 
     for ($i = 0; $i < 7; $i++) {
 
@@ -11,11 +11,11 @@ function drawAbacus(): array {
             
         if ($value != 0) {
 
-            $arrayNewLine = array();
+            $arrayNewLine = [];
 
             while (count($arrayNewLine)< $value) {
 
-                array_push($arrayNewLine, "0");
+                array_push($arrayNewLine, "O");
 
             }
 
@@ -26,7 +26,7 @@ function drawAbacus(): array {
 
             while (count($arrayNewLine)<12) {
 
-                array_push($arrayNewLine, "0");
+                array_push($arrayNewLine, "O");
 
             }
 
@@ -34,7 +34,7 @@ function drawAbacus(): array {
 
         } else {
 
-            $newLine = "---000000000";
+            $newLine = "---OOOOOOOOO";
         }
            
         array_push($abacus, $newLine);
@@ -49,25 +49,17 @@ function readAbacus(array $abacus): string{
 
     $totalValue = 0;
 
-    $numbers_array = array();
+    $numbers_array = [];
 
     foreach ($abacus as $line) {
          
-        $pos = strpos($line, "-");
-
-        $number = $pos;
-
-        array_push($numbers_array, $number);
+        array_push($numbers_array, strpos($line, "-"));
 
     }
         
-    $arrayReverse = array_reverse($numbers_array);
-
-    foreach ($arrayReverse  as $key => $value){
-            
-        $partialValue =  $value * pow(10, $key);
-
-        $totalValue = $totalValue + $partialValue ;
+    foreach (array_reverse($numbers_array) as $key => $value){
+             
+        $totalValue = $totalValue + $value * pow(10, $key);
 
     }
 

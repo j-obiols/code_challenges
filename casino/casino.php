ac081f3604dcce933c7bb67af414237cade6762a
Joana Obiols <joana.obiols@gmail.com>
2023-11-22 18:43:09 +0100

    New version redefining Casino class

--- casino/casino.php
+++ casino/casino.php
@@ -1,10 +1,88 @@
 <?php
 
-function status(array $casino){
 
-    foreach($casino as $player){
-        if($player -> inCasino == true) {
-            $player -> isWinning();
-        }
+
+class Casino{
+
+    private $players = array();
+
+
+    public function getPlayers() {
+        return $this -> players;
     }
+
+
+    public function addPlayer(Player $player) {
+        $this -> players[] = $player;
+    }
+
+
+    public function removePlayer(Player $player) {
+
+        foreach($this->players as $key => $value){
+            if($value == $player){
+                unset($this -> players[$key]);
+            }
+        }
+
+    }
+    
+
+    public function enters($player) {
+    
+        if($this -> inCasino($player)) {
+            echo $player -> getName()." is already in the Casino\n";
+        } else {
+            $this -> addPlayer($player);
+        }
+
+    }
+
+
+    public function leaves($player) {
+
+        if( $this -> inCasino($player)){
+            $player -> hasWon();
+            $player -> resetTotalAmount();
+            $this -> removePlayer($player);
+        } else {
+            echo $player -> getName()." is not in the Casino\n";
+        }
+
+    }
+
+
+    public function wins($player, $amount) {
+
+        if($this -> inCasino($player)){
+            $player-> totalAmount += $amount;
+        } else {
+            echo $player -> getName()." is not in the Casino\n";
+        }
+
+    }
+
+
+    public function inCasino($player) {
+
+        $players = $this -> getPlayers();
+
+        if(in_array($player, $players)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    function status() {
+        
+        $players = $this -> getPlayers();
+
+        foreach($players as $player){
+           echo $player -> isWinning(); 
+        }
+    } 
+
 } 

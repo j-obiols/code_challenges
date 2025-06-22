58fcc46f245d48a2cac14e1fc8c8ac4577702b7e
Joana Obiols <joana.obiols@gmail.com>
2025-06-22 22:17:36 +0200

    Clean function area. Add a new function for checking units

---
+++ rectangle_area/functions.php
@@ -0,0 +1,24 @@
+<?php 
+
+function check_unit($unit) {
+     
+    $units = array("km", "hm", "dam", "m", "dm", "cm", "mm");
+
+    if (is_numeric($unit) OR !in_array($unit, $units)) {
+
+        return false;
+
+    } else {  
+
+        return true;   
+
+    }
+}
+
+function area_rectangle($unit, $width, $height) {
+ 
+    $area = $width * $height;
+
+    echo "Area = ". $area." ".$unit. " 2 " ;  
+
+}
--- rectangle_area/index.php
+++ rectangle_area/index.php
@@ -1,5 +1,35 @@
-<?php
+<?php 
 
-require 'function.php';
+require 'functions.php';
 
-area_rectangle();
+
+echo "Introduce the length unit you want to use (km, hm, dam, m, dm, cm, mm):";
+
+$unit = strtolower(readline());
+
+$response = check_unit($unit);
+
+if ($response == false) {
+           
+    echo "Invalid data. Please try again";
+
+} else {
+       
+    echo "Introduce rectangle's width:";
+
+    $width = readline(); 
+
+    echo "Introduce rectangle's height:";
+
+    $height = readline(); 
+       
+    if (is_numeric($width) && $width > 0 && is_numeric($height) && $height > 0) {
+
+        area_rectangle($unit, $width, $height);
+
+    } else {
+       
+        echo "Invalid width and/or invalid length. Please start again."; 
+    }
+
+}
